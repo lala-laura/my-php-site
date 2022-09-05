f412ecadac311aefbc078871b2c122e6c94e8082
Laura Cornell-d'Echert <90943940+lala-laura@users.noreply.github.com>
2022-09-05 14:29:36 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,92 @@
+        <?php require 'includes/header.php'; ?>
+
+
+        <div class="title">
+            <h1 class="title-portfolio">PORTFOLIO</h1>
+            <h2 class="title-name">LAURA CORNELL-d'ECHERT</h2>
+        </div>
+    </header>
+    <!-- TEMP NOTE (DELETE) -->
+    <div class="in-progress">What's New<span id="dots">...</span>
+        <span id="cont"><br>
+            May 30, 2022 - Added "Graphics" page to Projects. Added Blog and Skills pages.
+        </span><br>
+        <button onclick="myFunction()" id="moreBtn">Read more</button>
+        <br>
+        <span style="font-size:14.5px; text-align: center; color:#f3b1dd;">
+            This site is a Work-In-Progress. Please excuse the mess.
+            <span style="font-size:22px">👩‍💻</span>
+        </span>
+    </div>
+    <!-- Temp Note End-->
+
+    <div class="info">
+        <!-- welcome -->
+        <div class="info-welcome">
+            <div class="welcome">
+
+                <h3>
+                    <div class="welcome-text">Welcome to my Portfolio Page!</div>
+                </h3>
+
+            </div>
+            <div class="welcome-img-mobile">
+                <img src="assets/img/laura-img.jpg" alt="" class="laura-img">
+            </div>
+        </div>
+
+
+        <!-- about -->
+        <div class="about">
+            <div class="about-grid">
+
+
+                <div class="about-text">
+                    <h4>ABOUT ME</h4>
+
+                    <p> As of December 2021, I completed the <i>Web Design and Development Certificate</i> program given by the School of Continuing and Professional Studies at Loyola University Chicago. I am currently working toward building more experience in web development, with a particular interest in transitioning into a Full Stack Developer.</p>
+                    <p>I have a bachelor&#x27;s degree from Florida Atlantic University. I've worked in an administrative role for a Virginia based non-profit for 13 years and worked remotely since 2018. My job skills have grown along with my responsibilities. I will add more information about my skills as I build this site but, among other things, I built and manage the organization's HTML email templates. I also have Salesforce Administrative duties for the company and have written and maintain complex Salesforce formulas for email merging and record keeping. </p>
+
+
+                    <p> As of late Summer 2020, I have been residing in Kansas City, KS with my husband and dog.</p>
+
+                </div>
+                <div class="about-img">
+
+                    <img src="assets/img/laura-img.jpg" alt="" class="laura-img">
+                </div>
+
+
+            </div>
+            <!-- contact -->
+            <section class="contact">
+                <h5>CONTACT</h5>
+                <div class="contact-grid">
+
+                    <div class="contact-email">
+                        <p>Contact me at
+                            <a href="mailto:laura.cornelldechert@gmail.com" class="email-address">laura.cornelldechert@gmail.com</a> </p>
+                    </div>
+
+                    <div class="contact-link">
+                        <a href="https://www.linkedin.com/in/laura-cornelldechert/" target="_blank"><img src="assets/img/linkedin-150.png" alt="linkedin icon" class="img-linkedin"></a>
+                    </div>
+                </div>
+            </section>
+        </div> <!-- closes 'about' div -->
+    </div> <!-- closes 'info' div -->
+   <?php include 'includes/footer.php' ?>
+
+    <!-- ---- FOOTER ---- -->
+    <!--   <footer> &copy;<script type="text/javascript">
+            document.write(new Date().getFullYear());
+        </script> Website Hand Coded by Laura Cornell-d'Echert
+    </footer>
+
+    <script src="./assets/js/index.js">
+    </script>
+    <script src="./assets/js/navigation.js"> </script> -->
+    <!-- ---- FOOTER - php-test ---- -->
+
+
+
